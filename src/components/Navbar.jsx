09af0e4b1e60c2ed2cbf0ed54f7c7845450fd545
asketsystem <asketsystem1@gmail.com>
2022-01-16 13:39:45 +0000

    Create React Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,25 +8,59 @@ const Container = styled.div`
 const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
+    align-items: center;
     justify-content: space-between;
 `;
 
 const Left = styled.div`
     width: 60%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 `;
 
 const Logo = styled.h1`
     font-weight: bold;
-`
+    text-decoration: underline crimson;
+`;
+
+const Menu = styled.ul`
+    display: flex;
+    list-style: none;
+`;
+const MenuItem = styled.li`
+    margin-right: 30px;
+    font-size: 20px;
+    font-weight: bold;
+    color: gray;
+`;
+
+const Button = styled.button`
+    border: 2px solid white;
+    padding: 10px 15px;
+    background-color: crimson;
+    color: white;
+    font-weight: bold;
+    border-radius: 10px;
+    cursor: pointer;
+`;
 
 const Navbar = () => {
     return (
         <Container>
             <Wrapper>
-                <span>logo</span>
-                <span>button</span>
+            <Left>
+                <Logo>Asketstudio</Logo>
+                <Menu>
+                    <MenuItem>Home</MenuItem>
+                    <MenuItem>Features</MenuItem>
+                    <MenuItem>Services</MenuItem>
+                    <MenuItem>Pricing</MenuItem>
+                    <MenuItem>Contact</MenuItem>
+                </Menu>
+            </Left>
+                <Button>JOIN TODAY </Button>
             </Wrapper>
-            
         </Container>
     )
 }
