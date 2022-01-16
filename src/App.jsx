47f23de3441c49b42ec50b8cc1f8c096c2f60588
asketsystem <asketsystem1@gmail.com>
2022-01-16 18:40:13 +0000

    Intro Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import styled from "styled-components"
 import Navbar from "./components/Navbar";
+import Intro from "./components/Intro";
 
 const Container = styled.div`
   height: 100vh;
+  overflow: hidden;
 `;
 
 const App = () => {
   return (
     <Container>
       <Navbar/>
+      <Intro/>
     </Container>
   )
 };
