1b066beb18ee611ad4bb1bd67ca47a8b1ae28d36
asketsystem <asketsystem1@gmail.com>
2022-01-17 15:43:36 +0000

    React Feature Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 import Navbar from "./components/Navbar";
 import Intro from "./components/Intro";
+import Feature from "./components/Feature";
 
 const Container = styled.div`
   height: 100vh;
   overflow: hidden;
+  position: relative;
+`;
+
+const Shape = css`
+width: 100%;
+height: 100%;
+position: absolute;
+top: 0;
+left: 0;
+z-index: -1;
 `;
 
 const IntoShape = styled.div`
+  ${Shape}
   width: 100%;
   height: 100%;
   position: absolute;
@@ -18,13 +30,31 @@ const IntoShape = styled.div`
   background-color: orange;
 `;
 
+const FeatureShape = styled.div`
+  ${Shape}
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  clip-path: polygon(0 0, 55% 0%, 33% 100%, 0 100%);
+  background-color: pink;
+`;
+
 const App = () => {
   return (
-    <Container>
-      <Navbar/>
-      <Intro/>
-      <IntoShape/>
-    </Container>
+    <>
+      <Container>
+        <Navbar/>
+        <Intro/>
+        <IntoShape/>
+      </Container>
+      <Container>
+        <Feature/>
+        <FeatureShape/>
+      </Container>
+    </>
   )
 };
 
